3156b8ee463f33ce7fa18866c3cc20f74a05a6f4
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2025-06-04 20:11:33 -0300

    AB#249 update buscar

--- public/buscar.js
+++ public/buscar.js
@@ -5,17 +5,16 @@ form.addEventListener('submit', async (e) => {
 
   const formData = new FormData(form);
   const data = Object.fromEntries(formData.entries());
-
   const cpf = data.cpf;
 
   try {
     const res = await fetch(`http://localhost:3000/api/participants/buscar?cpf=${encodeURIComponent(cpf)}`);
     const participante = await res.json();
+    console.log(participante);
 
     const lista = document.getElementById('resultados');
     lista.innerHTML = '';
 
-    // Verifica se houve erro na resposta
     if (!res.ok || participante.error || !participante) {
       const item = document.createElement('li');
       item.classList.add('list-group-item');
@@ -24,7 +23,12 @@ form.addEventListener('submit', async (e) => {
     } else {
       const item = document.createElement('li');
       item.classList.add('list-group-item');
-      item.textContent = `${participante.nome} - ${participante.email} - CPF: ${participante.cpf}`;
+
+      const statusHTML = participante.presenca
+        ? '<span class="badge bg-success">Presença confirmada ✅</span>'
+        : '<span class="badge bg-secondary">Presença pendente ⏳</span>';
+
+      item.innerHTML = `${participante.nome} - ${participante.email} - CPF: ${participante.cpf} - ${statusHTML}`;
       lista.appendChild(item);
     }
   } catch (err) {
